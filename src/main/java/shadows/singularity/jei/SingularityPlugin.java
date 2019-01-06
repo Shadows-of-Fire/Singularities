26b15bd9dcf60de3af9cbc385466b39d62b6fa65
Shadows_of_Fire <Bward7864@gmail.com>
2019-01-06 01:08:17 -0500

    Fix JEI when compressor is hidden

--- src/main/java/shadows/singularity/jei/SingularityPlugin.java
+++ src/main/java/shadows/singularity/jei/SingularityPlugin.java
@@ -8,6 +8,7 @@ import net.minecraft.item.ItemStack;
 import shadows.singularity.Singularities;
 import shadows.singularity.recipe.CompressorManager;
 import shadows.singularity.recipe.ICompressorRecipe;
+import shadows.singularity.recipe.SingularityConfig;
 
 @JEIPlugin
 public class SingularityPlugin implements IModPlugin {
@@ -16,11 +17,12 @@ public class SingularityPlugin implements IModPlugin {
 
 	@Override
 	public void registerCategories(IRecipeCategoryRegistration reg) {
-		reg.addRecipeCategories(new CompressorCategory(reg.getJeiHelpers().getGuiHelper()));
+		if (!SingularityConfig.hideCompressor) reg.addRecipeCategories(new CompressorCategory(reg.getJeiHelpers().getGuiHelper()));
 	}
 
 	@Override
 	public void register(IModRegistry reg) {
+		if (SingularityConfig.hideCompressor) return;
 		reg.handleRecipes(ICompressorRecipe.class, CompressorWrapper::new, C_UID);
 		reg.addRecipeCatalyst(new ItemStack(Singularities.COMPRESSOR), C_UID);
 		reg.addRecipes(CompressorManager.getValidRecipes(), C_UID);

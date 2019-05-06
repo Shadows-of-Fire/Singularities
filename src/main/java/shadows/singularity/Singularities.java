8f095c16c3cfa0c25e15e6581931f3e585a69fc1
Shadows_of_Fire <Bward7864@gmail.com>
2019-05-06 01:16:30 -0400

    bump ver

--- src/main/java/shadows/singularity/Singularities.java
+++ src/main/java/shadows/singularity/Singularities.java
@@ -36,7 +36,7 @@ public class Singularities {
 
 	public static final String MODID = "singularities";
 	public static final String MODNAME = "Singularities";
-	public static final String VERSION = "2.4.0";
+	public static final String VERSION = "2.4.1";
 	public static final String DEPS = "required-after:placebo@[1.2.0,);before:crafttweaker;after:avaritia";
 
 	public static final CreativeTabs TAB = new CreativeTabs(MODID) {
